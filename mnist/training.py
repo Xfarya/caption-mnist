82d613a46c95e850611da8dbf5458c89a948f8cb
Xfarya <farya243@gmail.com>
2024-11-13 16:54:31 +0000

    completed training loop

--- mnist/training.py
+++ mnist/training.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 import torch
 import torch.nn as nn
 
-from mnist.encoderdecoder import Decoder, Encoder
+from encoderdecoder import Decoder, Encoder
 
 vocab_size = 10
 dim_size = 64
@@ -13,23 +13,35 @@ batch_size = 1
 encoder = Encoder(vocab_size, dim_size)
 decoder = Decoder(vocab_size, dim_size)
 
-
-input_sequence = torch.tensor([[1, 2, 3, 4]])
+input_sequence = torch.tensor([[1, 2, 3, 4, 5]])
 target_sequence = torch.tensor([[2, 3, 4, 5, 6]])
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=0.001)
 
-encoded_output = encoder(input_sequence)
+num_epochs = 10
 
-decoder_input = torch.tensor([[0]])
+for epoch in range(num_epochs):
+    encoder.train()
+    decoder.train()
+    optimizer.zero_grad()
 
-encoder.train()
-decoder.train()
-optimizer.zero_grad()
+    encoded_output = encoder(input_sequence)
 
-output_logits = decoder(decoder_input, encoded_output)
+    decoder_input = torch.tensor([[0]]) #<s>
+    print(decoder_input)
 
+    output_logits = []
+
+for i in range(target_sequence.size(1)):
+    logits = decoder(decoder_input, encoded_output)
+
+    output_logits.append(logits)
+
+    _, next_token = logits.max(dim=-1)
+    decoder_input = next_token
+
+output_logits = torch.cat(output_logits, dim=1)
 output_logits = output_logits.view(-1, vocab_size)
 target_sequence = target_sequence.view(-1)
 
@@ -38,4 +50,4 @@ loss = criterion(output_logits, target_sequence)
 loss.backward()
 optimizer.step()
 
-print("Loss:", loss.item())
+print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
